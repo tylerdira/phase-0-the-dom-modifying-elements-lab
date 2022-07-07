8c92bfe14e61ffc3823851776ede3389e1e45da4
Joseph Tyler DiRaffaele <tylerdiraffaele@gmail.com>
2022-07-07 10:20:16 -0700

    commit tree a2cdea0ec05762ac5cded065247f876f86d5d2fa

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+// const ul = document.createElement("ul");
+//       for (let i = 0; i < 3; i++) {
+//         const li = document.createElement("li");
+//         li.textContent = (i + 1).toString();
+//         ul.append(li);
+//       }
+
+document.getElementById("main").remove()
+
+const newHeader = document.createElement('h1')
+newHeader.textContent = 'Tyler is the champion'
+newHeader.id = 'victory'
+document.body.append(newHeader)
+
+
+
+// const main = document.getElementById("main")
+// main.remove()
